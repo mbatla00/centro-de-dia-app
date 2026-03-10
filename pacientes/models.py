cb929546164224c2d1a65007a636a49400f5680c
mbatla00 <mbatla00@estudiantes.unileon.es>
2026-03-10 18:00:07 +0100

    Añadir modelo Paciente

--- pacientes/models.py
+++ pacientes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Paciente(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellidos = models.CharField(max_length=150)
+    fecha_nacimiento = models.DateField()
+    telefono = models.CharField(max_length=20, blank=True)
+    direccion = models.CharField(max_length=200, blank=True)
+
+    fecha_registro = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellidos}"
