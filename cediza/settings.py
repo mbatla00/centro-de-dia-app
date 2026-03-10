4e904ffce41a123f9b3750844cf0ddf43494f4d4
mbatla00 <mbatla00@estudiantes.unileon.es>
2026-03-10 17:38:08 +0100

    Configurar Django

--- cediza/settings.py
+++ cediza/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'usuarios',
+    'pacientes',
+    'cuestionarios',
+    'anotaciones',
+    'informes',
 ]
 
 MIDDLEWARE = [
